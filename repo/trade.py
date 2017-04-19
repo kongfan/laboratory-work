b91f0ff362ffc69c57fe04a79669e26ddcee4b10
kongfan <kongfan@outlook.com>
2017-04-19 23:04:54 +0800

    trade.py

--- repo/trade.py
+++ repo/trade.py
@@ -7,12 +7,14 @@ This is a python script for trade.
 """
 
 import time
-import easytrader
+#import easytrader
 import tushare as ts
 
-
-user = easytrader.use('yh', debug=False)
-user.prepare('yh.json')
+try:
+    #user = easytrader.use('yh', debug=False)
+    #user.prepare('yh.json')
+except:
+    print u"登录失败!"
 
 buy_times = 0
 sell_times = 0
@@ -24,26 +26,29 @@ retracement = 0.001       # 从高位回撤的值
 
 i = 1
 while True:
-    snap = ts.get_realtime_quotes(symbols)
+    try:
+        snap = ts.get_realtime_quotes(symbols)
 
-    average = float(snap.amount[0])/float(snap.volume[0])   # 当日实时均价
-    unit_amount = int(1500/float(snap.price[0])/100)*100    # 单次买卖的数量
-    shortNM = snap.name[0]                                  # 证劵简称
-    openPrice = float(snap.name[0])                         # 今日开盘价
-    prevClosePrice = float(snap.name[0])                    # 昨收盘价格
-    lastPrice = float(snap.price[0])                        # 最新价格
-    highPrice = float(snap.high[0])                         # 最高价格
-    lowPrice = float(snap.low[0])                           # 最低价格
-    volume = float(snap.volume[0])                          # 成交数量
-    value = float(snap.amount[0])                           # 成交金额
-    bidBook_volume1 = float(snap.b1_v[0])                   # 一档买入交易量
-    bidBook_price1 = float(snap.b1_p[0])                    # 一档买入价格
-    askBook_volume1 = float(snap.a1_v[0])                   # 一档卖出交易量
-    askBook_price1 = float(snap.a1_p[0])                    # 一档卖出价格
-    dataDate = float(snap.date[0])                          # 交易时间
-    dataTime = float(snap.time[0])                          # 交易时间'HH:mm:ss'
-    average = round(value / volume, 3)                      # 当日均价
-    amplitude = round((lastPrice - average) / average, 5)*100  # 实时振幅
+        average = float(snap.amount[0])/float(snap.volume[0])   # 当日实时均价
+        unit_amount = int(1500/float(snap.price[0])/100)*100    # 单次买卖的数量
+        shortNM = snap.name[0]                                  # 证劵简称
+        openPrice = float(snap.name[0])                         # 今日开盘价
+        prevClosePrice = float(snap.name[0])                    # 昨收盘价格
+        lastPrice = float(snap.price[0])                        # 最新价格
+        highPrice = float(snap.high[0])                         # 最高价格
+        lowPrice = float(snap.low[0])                           # 最低价格
+        volume = float(snap.volume[0])                          # 成交数量
+        value = float(snap.amount[0])                           # 成交金额
+        bidBook_volume1 = float(snap.b1_v[0])                   # 一档买入交易量
+        bidBook_price1 = float(snap.b1_p[0])                    # 一档买入价格
+        askBook_volume1 = float(snap.a1_v[0])                   # 一档卖出交易量
+        askBook_price1 = float(snap.a1_p[0])                    # 一档卖出价格
+        dataDate = float(snap.date[0])                          # 交易时间
+        dataTime = float(snap.time[0])                          # 交易时间'HH:mm:ss'
+        average = round(value / volume, 3)                      # 当日均价
+        amplitude = round((lastPrice - average) / average, 5)*100  # 实时振幅
+    except:
+        print u"获取行情错误!"
 
     if (lastPrice < average*(1-deviation) and
             lastPrice > lowPrice + retracement):
@@ -53,8 +58,8 @@ while True:
               average*(1-deviation), u" | 偏幅:", amplitude)
 
         if buy_times < 1:
-            user.buy(symbols, price=bidBook_price1 + 0.001,
-                     amount=unit_amount)
+            #user.buy(symbols, price=bidBook_price1 + 0.001,
+            #         amount=unit_amount)
 
             buy_times = buy_times + 1
 
@@ -73,8 +78,8 @@ while True:
         )
 
         if sell_times < 1:
-            user.sell(symbols, price=askBook_price1 - 0.001,
-                      amount=unit_amount)
+            #user.sell(symbols, price=askBook_price1 - 0.001,
+            #          amount=unit_amount)
 
             sell_times = sell_times + 1
             with open('deal.csv', 'a') as f:
@@ -90,10 +95,10 @@ while True:
     i = i+1
     time.sleep(3)
 
-    if (time.strftime("%H:%M", time.localtime(time.time())) == "14:56" and
-            user.position[0][u"current_amount"] >= unit_amount * 2):
+    if time.strftime("%H:%M", time.localtime(time.time())) == "14:56": #and
+            #user.position[0][u"current_amount"] >= unit_amount * 2):
 
-        user.sell(symbols, price=snap.bid[0], amount=unit_amount)
+        #user.sell(symbols, price=snap.bid[0], amount=unit_amount)
         with open('deal.csv', 'a') as f:
             f.write(dataDate, dataTime, 'sell', symbols, unit_amount,
                     (bidBook_price1 - 0.001), '收盘清仓 \n')
