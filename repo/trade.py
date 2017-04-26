9734ee44b77fa03dc0111b70b97484ad267f04f8
kongfan <kongfan@outlook.com>
2017-04-26 17:21:25 +0800

    new file:   repo/222.csv

--- repo/trade.py
+++ repo/trade.py
@@ -12,18 +12,21 @@ import time
 import easytrader
 import tushare as ts
 
-with open('deal.csv', 'a') as f:
-    mesg = time.strftime("%Y-%m-%d", time.localtime(time.time()))
-    f.write(mesg)
 
 try:
-        user = easytrader.use('yh', debug=False)
-        user.prepare('yh.json')
+    user = easytrader.use('yh', debug=False)
+    user.prepare('yh.json')
+    with open('deal.csv', 'a') as f:
+        mesg = u"登录成功! "+time.strftime("%Y-%m-%d", time.localtime(time.time())
+                                      ), user.position[0]  # [u"current_amount"]
+        f.write(mesg)
+
 except Exception as e:
-        print u"登录失败!", e
-        with open('deal.csv', 'a') as f:
-            mesg = time.strftime("%Y-%m-%d", time.localtime(time.time())), e
-            f.write(mesg)
+    print u"登录失败!", e
+    with open('deal.csv', 'a') as f:
+        mesg = u"登录失败! "+time.strftime("%Y-%m-%d",
+                                      time.localtime(time.time())), e
+        f.write(mesg)
 
 buy_times = 0
 sell_times = 0
@@ -33,89 +36,122 @@ retracement = 0.001       # 从高位回撤的值
 
 i = 1
 while True:
-        snap = ts.get_realtime_quotes(symbols)
-        unit_amount = int(1500/float(snap.price[0])/100)*100    # 单次买卖的数量
+    snap = ts.get_realtime_quotes(symbols)
+    unit_amount = int(1500/float(snap.price[0])/100)*100    # 单次买卖的数量
 
-        shortNM = snap.name[0]                                  # 证劵简称
-        openPrice = float(snap.open[0])                         # 今日开盘价
-        prevClosePrice = float(snap.pre_close[0])               # 昨收盘价格
-        lastPrice = float(snap.price[0])                        # 最新价格
-        highPrice = float(snap.high[0])                         # 最高价格
-        lowPrice = float(snap.low[0])                           # 最低价格
-        volume = float(snap.volume[0])                          # 成交数量
-        value = float(snap.amount[0])                           # 成交金额
-        bidBook_volume1 = float(snap.b1_v[0])                   # 一档买入交易量
-        bidBook_price1 = float(snap.b1_p[0])                    # 一档买入价格
-        askBook_volume1 = float(snap.a1_v[0])                   # 一档卖出交易量
-        askBook_price1 = float(snap.a1_p[0])                    # 一档卖出价格
-        dataDate = snap.date[0]                                 # 交易时间
-        dataTime = snap.time[0]                                 # 交易时间'HH:mm:ss'
-        average = round(value / volume, 3)                      # 当日均价
-        amplitude = round((lastPrice - average) / average, 5)*100  # 实时振幅
-        updeviation = round(average*(1+deviation), 3)
-        downdeviation = round(average*(1-deviation), 3)
+    shortNM = snap.name[0]                                  # 证劵简称
+    openPrice = float(snap.open[0])                         # 今日开盘价
+    prevClosePrice = float(snap.pre_close[0])               # 昨收盘价格
+    lastPrice = float(snap.price[0])                        # 最新价格
+    highPrice = float(snap.high[0])                         # 最高价格
+    lowPrice = float(snap.low[0])                           # 最低价格
+    volume = float(snap.volume[0])                          # 成交数量
+    value = float(snap.amount[0])                           # 成交金额
+    bidBook_volume1 = float(snap.b1_v[0])                   # 一档买入交易量
+    bidBook_price1 = float(snap.b1_p[0])                    # 一档买入价格
+    askBook_volume1 = float(snap.a1_v[0])                   # 一档卖出交易量
+    askBook_price1 = float(snap.a1_p[0])                    # 一档卖出价格
+    dataDate = snap.date[0]                                 # 交易时间
+    dataTime = snap.time[0]                                 # 交易时间'HH:mm:ss'
+    average = round(value / volume, 3)                      # 当日均价
+    amplitude = round((lastPrice - average) / average, 5)*100  # 实时振幅
+    updeviation = round(average*(1+deviation), 3)
+    downdeviation = round(average*(1-deviation), 3)
 
-        if (lastPrice < average*(1-deviation) and
-                lastPrice > lowPrice + retracement):
-                print("%d %s %s | 现价: %f  均价: %f | 卖点: %f  买点: %f | 偏幅: %f" % (
-                        i, shortNM, dataTime, lastPrice, average, updeviation, downdeviation, amplitude))
-                if buy_times < 1:
-                        try:
-                                user.buy(symbols, price=bidBook_price1 + 0.001,
-                                         amount=unit_amount)
-                                buy_times = buy_times + 1
-                                with open('deal.csv', 'a') as f:
-                                        mesg = (dataDate + ";" + dataTime +
-                                                ';buy;' + symbols + ";" +
-                                                unit_amount + ";" +
-                                                (bidBook_price1 + 0.001) + '\n')
-                                        f.write(mesg)
-                                print "buy at:", (bidBook_price1 + 0.001), "total:", buy_times
-                        except Exception as e:
-                                print "买入下单出错！", e
-        elif (lastPrice > average * (1 + deviation) and
-              lastPrice < highPrice - retracement):
-                print("%d %s %s | 现价: %f  均价: %f | 卖点: %f  买点: %f | 偏幅: %f" % (
-                        i, shortNM, dataTime, lastPrice, average, updeviation, downdeviation, amplitude))
-                if sell_times < 1:
-                        try:
-                                user.sell(symbols, price=askBook_price1 - 0.001,
-                                          amount=unit_amount)
-                                sell_times = sell_times + 1
-                                with open('deal.csv', 'a') as f:
-                                        mesg = (dataDate + ";" + dataTime +
-                                                ';buy;' + symbols + ";" +
-                                                unit_amount + ";" +
-                                                (bidBook_price1 + 0.001) + '\n')
-                                        f.write(mesg)
-                                print "sell at:", (askBook_price1 - 0.001), "total:", sell_times
-                        except Exception as e:
-                                print "卖出下单出错！", e
-        else:
-                print("%d %s %s | 现价: %f  均价: %f | 卖点: %f  买点: %f | 偏幅: %f" % (
-                        i, shortNM, dataTime, lastPrice, average, updeviation, downdeviation, amplitude))
-
-        i = i+1
-        time.sleep(3)
-
-        if (time.strftime("%H:%M", time.localtime(time.time())) == "14:56" and
-                user.position[0][u"current_amount"] >= unit_amount * 2):
-                user.sell(
-                    symbols, price=(bidBook_price1 - 0.001),
-                    amount=unit_amount)
+    if (lastPrice < average*(1-deviation) and
+            lastPrice > lowPrice + retracement):
+        print(
+            "buy-- %d %s %s | 现价: %.3f  均价: %.3f | 卖点: %.3f  买点: %.3f | 偏幅: %.3f" %
+            (i,
+             shortNM,
+             dataTime,
+             lastPrice,
+             average,
+             updeviation,
+             downdeviation,
+             amplitude))
+        if buy_times < 1:
+            try:
+                user.buy(symbols, price=bidBook_price1 + 0.001,
+                         amount=unit_amount)
+                buy_times = buy_times + 1
                 with open('deal.csv', 'a') as f:
-                        mesg = (dataDate +
-                                ";" +
-                                dataTime +
-                                ';sell;' +
-                                symbols +
-                                ";" +
-                                unit_amount +
-                                ";" +
-                                (askBook_price1 -
-                                 0.001) +
-                                ';收盘清仓 \n')
-                        f.write(mesg)
-                break
+                    mesg = (dataDate + ";" + dataTime +
+                            ';buy;' + symbols + ";" +
+                            unit_amount + ";" +
+                            (bidBook_price1 + 0.001) + '\n')
+                    f.write(mesg)
+                print "buy at:", (bidBook_price1 + 0.001), "total:", buy_times
+            except Exception as e:
+                print "买入下单出错！", e
+    elif (lastPrice > average * (1 + deviation) and
+          lastPrice < highPrice - retracement):
+        print(
+            "sell-- %d %s %s | 现价: %.3f  均价: %.3f | 卖点: %.3f  买点: %.3f | 偏幅: %.3f" %
+            (i,
+             shortNM,
+             dataTime,
+             lastPrice,
+             average,
+             updeviation,
+             downdeviation,
+             amplitude))
+        if sell_times < 1:
+            try:
+                user.sell(symbols, price=askBook_price1 - 0.001,
+                          amount=unit_amount)
+                sell_times = sell_times + 1
+                with open('deal.csv', 'a') as f:
+                    mesg = (dataDate + ";" + dataTime +
+                            ';buy;' + symbols + ";" +
+                            unit_amount + ";" +
+                            (bidBook_price1 + 0.001) + '\n')
+                    f.write(mesg)
+                print "sell at:", (askBook_price1 - 0.001), "total:", sell_times
+            except Exception as e:
+                print "卖出下单出错！", e
+    else:
+        print(
+            "keep-- %d %s %s | 现价: %.3f  均价: %.3f | 卖点: %.3f  买点: %.3f | 偏幅: %.3f" %
+            (i,
+             shortNM,
+             dataTime,
+             lastPrice,
+             average,
+             updeviation,
+             downdeviation,
+             amplitude))
+
+    i = i+1
+    time.sleep(3)
+
+    if (time.strftime("%H:%M", time.localtime(time.time())) == "14:56" and
+            user.position[0][u"current_amount"] >= unit_amount * 1.8):
+        user.sell(
+            symbols, price=(bidBook_price1 - 0.001),
+            amount=unit_amount)
+        with open('deal.csv', 'a') as f:
+            mesg = (dataDate +
+                    ";" +
+                    dataTime +
+                    ';sell;' +
+                    symbols +
+                    ";" +
+                    unit_amount +
+                    ";" +
+                    (askBook_price1 -
+                     0.001) +
+                    ';收盘清仓 \n')
+            f.write(mesg)
+        break
+
+    if time.strftime("%H:%M", time.localtime(time.time())) == "14:58":
+        break
+
+with open('deal.csv', 'a') as f:
+    mesg = time.strftime("%Y-%m-%d", time.localtime(time.time())
+                            ), user.position[0]
+    f.write(mesg)
+
 print time.strftime("%Y-%m-%d", time.localtime(time.time()))+"日内交易策略完成！"
 time.sleep(120)
